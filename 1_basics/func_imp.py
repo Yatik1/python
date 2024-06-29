f3a63a5355a28019aecc186a501c707c4ee5ca7c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-29 23:31:02 +0530

    python basics

---
+++ 1_basics/func_imp.py
@@ -0,0 +1,3 @@
+from hello_world import intro   ## importing function "intro" from hello_world file
+
+intro("Hello , function is imported from different file")
---
+++ 1_basics/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello World , learning python begin !!")
+
+def intro(name):
+    print(name)
+    
+intro("my name is yatik")
+
+city='delhi'
+addres='raiut'
