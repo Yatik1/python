14ec33f2863f34ee7e1d029f5b60867ce83a3c05
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-02 13:20:03 +0530

    10

---
+++ 02_conditional/sol10.py
@@ -0,0 +1,13 @@
+pet = "Cat"
+pet_age = 8
+
+if pet == "Dog" : 
+    if pet_age <=2:
+        print("Puppy Food")
+    else:
+        print("Adult dog Food")
+elif pet == "Cat":
+    if pet_age > 5:
+        print("Senior Cat Food")
+    else:
+        print("Kitten Food")
