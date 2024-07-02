8f933287cb35048bd89e73d2a38a8d594e81a772
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-02 14:21:00 +0530

    7

---
+++ 03_iteration/sol7.py
@@ -0,0 +1,7 @@
+while True : 
+    number = int(input("Enter number between 1 and 10 : "))
+    if 1<=number<=10:
+        print("Thanks for the input number")
+        break
+    else:
+        print("Invalid number , try again")
