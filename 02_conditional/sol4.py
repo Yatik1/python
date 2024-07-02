17756564fef7ea6803b30b3879a28932e8954542
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-02 13:19:26 +0530

    4

---
+++ 02_conditional/sol4.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unique")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
