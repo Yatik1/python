2e423afbc48947c9eef3845204b0f2715cdeae40
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-02 14:20:36 +0530

    1

---
+++ 03_iteration/sol1.py
@@ -0,0 +1,8 @@
+numbers = [1,-2,3,-4,5,6,-7,-8,9,10]
+
+postitive_numbers = 0
+
+for num in numbers:
+    if num > 0:
+        postitive_numbers+=1
+print("Count of Positive Number is : " , postitive_numbers)
