28528e7d43cb20a7efc123c27b73e10f659e6740
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-25 18:53:56 +0530

    :: functional structure

---
+++ 10_database_sqlite3/video_manager_db.py
@@ -0,0 +1,50 @@
+
+def list_all_videos():
+    pass
+
+def add_video(name,time):
+    pass
+
+def update_video(video_id,name,time):
+    pass
+
+def delete_video(video_id):
+    pass
+
+def main():
+    while True:
+        print("-" *70)
+        print(f" {' ' * 25 } VIDEO MANAGER")
+        print("-" *70 , "\n")
+
+        print("Choose the following choice: ")
+        print("1. List all the video you want to watch")
+        print("2. Add a video")
+        print("3. Update video details")
+        print("4. Delete a video")
+        print("5. Exit the app")
+
+        choice = input("Enter your choice : ")
+
+        match choice:
+            case "1":
+                list_all_videos()
+            case "2":
+                name=input("Enter name of video : ")
+                time=input("Enter time duration of the video : ")
+                add_video(name,time)
+            case "3":
+                video_id = input('Enter video id you wish to update : ')
+                name=input("Enter updated name of video : ")
+                time=input("Enter updated time duration of the video : ")
+                update_video(video_id,name,time)
+            case "4":
+                video_id = input('Enter video id you wish to delete : ')
+                delete_video(video_id)
+            case "5":
+                break
+            case _:
+                print("Invalid Choice")
+        
+if __name__ == "__main__":
+    main()
